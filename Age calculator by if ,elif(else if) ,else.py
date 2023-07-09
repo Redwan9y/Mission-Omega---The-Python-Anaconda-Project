d32b1107eaed025b82cb241f3c8f98b82cb7f0f5
Redwan9y <125296911+Redwan9y@users.noreply.github.com>
2023-07-10 00:58:47 +0600

    Create Age calculator by if ,elif(else if) ,else.py

---
+++ Age calculator by if ,elif(else if) ,else.py
@@ -0,0 +1,12 @@
+#Age calculator by Redwan Tylar.
+'''In this program we calculate our age'''
+age = 1000
+
+if(age <= 13):
+    print("Kid")
+elif(age <= 18):
+    print("iTeen")
+elif(age <=100):
+    print("Adult 18+")
+else :
+    print("Death")
